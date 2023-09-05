88cde5822977cef5421dc6e96acf7494dff1841f
kevintao <kevin1258@foxmail.com>
2023-09-05 17:30:09 +0800

    [web] fixed firebase

--- client/web/src/utils/firebase.js
+++ client/web/src/utils/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRqFxecZYe87DnYh7gYyzY_goAKmhrpV8",
-  authDomain: "realchar-a58de.firebaseapp.com",
-  projectId: "realchar-a58de",
-  storageBucket: "realchar-a58de.appspot.com",
-  messagingSenderId: "329881922764",
-  appId: "1:329881922764:web:86620c703c32f2a608a669",
-  measurementId: "G-CN8CGSZV9M"
+  apiKey: 'AIzaSyBRqFxecZYe87DnYh7gYyzY_goAKmhrpV8',
+  authDomain: 'realchar-a58de.firebaseapp.com',
+  projectId: 'realchar-a58de',
+  storageBucket: 'realchar-a58de.appspot.com',
+  messagingSenderId: '329881922764',
+  appId: '1:329881922764:web:86620c703c32f2a608a669',
+  measurementId: 'G-CN8CGSZV9M',
 };
 
 // Initialize Firebase
